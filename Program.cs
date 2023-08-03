ee733bd167b5be5bbd31b22609577d8ce19028f1
WebDev <leandranm@outlook.com>
2023-08-03 04:23:13 +0200

    Create Player 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
 ﻿namespace DiceGame;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int playerRandomNum;
+
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Press any key to roll the dice");
+
+            Console.ReadKey();
+
+            playerRandomNum = random.Next(1, 7);
+            Console.WriteLine("You rolled a " + playerRandomNum);
+        }
+        Console.ReadKey();
     }
 }
